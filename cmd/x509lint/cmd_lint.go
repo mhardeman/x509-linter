d4d968a8a2edf97f01c64882c5c396759ac5898b
microshine <microshine@mail.ru>
2022-09-23 01:21:47 +0300

    feat: use MD output

--- cmd/x509lint/cmd_lint.go
+++ cmd/x509lint/cmd_lint.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/url"
 	"os"
 	"path"
+	"time"
 
 	"github.com/peculiarventures/x509-linter/pkg/atis1000080"
 	"github.com/zmap/zcrypto/x509"
@@ -38,39 +43,19 @@ func RunLintCommand(certPath string) error {
 			log.Fatal(err)
 		}
 
-		counter := map[string]int{}
-		allCerts := 0
-		invalidCerts := 0
+		fmt.Println("# SHAKEN lint report")
+		fmt.Println("## Certificates")
 
 		for _, file := range files {
 			if !file.IsDir() {
+
 				filePath := path.Join(certPath, file.Name())
 				result, err := doLint(filePath)
 				if err != nil {
 					continue
 				}
-				allCerts = allCerts + 1
-				if result.ErrorsPresent || result.WarningsPresent || result.FatalsPresent {
-					invalidCerts = invalidCerts + 1
-					fmt.Println(filePath)
-					printLintResult(result)
-					for code, v := range result.Results {
-						if v.Status != lint.Pass {
-							counter[code] = counter[code] + 1
-						}
-					}
-				}
-			}
-		}
-
-		// Print common report
-		log.Printf("%s;%s\n", "Name", "Amount")
-		log.Printf("%s;%d\n", "Verified", allCerts)
-		log.Printf("%s;%d\n", "Invalid", invalidCerts)
-		if len(counter) > 0 {
-			log.Printf("%s;%s\n", "Code", "Amount")
-			for code, v := range counter {
-				log.Printf("%s;%d\n", code, v)
+				fmt.Println("")
+				printResultMarkDown(result)
 			}
 		}
 	} else {
@@ -79,28 +64,20 @@ func RunLintCommand(certPath string) error {
 		if err != nil {
 			return fmt.Errorf("cannot lint the certificate, %w", err)
 		}
-		printLintResult(result)
+
+		printResultMarkDown(result)
 	}
 
 	return nil
 }
 
-func printLine(code string, status string, description string) {
-	log.Printf("%s;%s;%s\n", code, status, description)
+type LintResult struct {
+	Link   string
+	Cert   *x509.Certificate
+	Result *zlint.ResultSet
 }
 
-func printLintResult(result *zlint.ResultSet) {
-	log.Default().SetFlags(0)
-	log.Default().SetOutput(os.Stdout)
-	printLine("Code", "Status", "Description")
-	for i, v := range result.Results {
-		if v.Status == lint.Error || v.Status == lint.Warn {
-			printLine(i, v.Status.String(), v.Details)
-		}
-	}
-}
-
-func doLint(certPath string) (*zlint.ResultSet, error) {
+func doLint(certPath string) (*LintResult, error) {
 	raw, err := os.ReadFile(certPath)
 	if err != nil {
 		return nil, fmt.Errorf("cannot read the certificate file %s, %s", certPath, err.Error())
@@ -133,13 +110,41 @@ func doLint(certPath string) (*zlint.ResultSet, error) {
 	}
 
 	config, err := lint.NewConfigFromString(`
-[w_organization_identity]
-Some = "test"
-`)
+	[w_organization_identity]
+	Some = "test"
+	`)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create config, error: %s", err.Error())
 	}
 	registry.SetConfiguration(config)
 
-	return zlint.LintCertificateEx(cert, registry), nil
+	link := p.Headers["Link"]
+	if link == "" {
+		link = certPath
+	}
+
+	return &LintResult{
+		Link:   link,
+		Cert:   cert,
+		Result: zlint.LintCertificateEx(cert, registry),
+	}, nil
+}
+
+func printResultMarkDown(info *LintResult) {
+	thumbprint := sha1.New()
+	thumbprint.Write(info.Cert.Raw)
+	fmt.Printf("### Certificate %s\n", hex.EncodeToString(thumbprint.Sum(nil)))
+	fmt.Printf("Timestamp: %s\n\n", time.Unix(info.Result.Timestamp, 0).String())
+	fmt.Printf("Subject: %s\n\n", info.Cert.Subject.String())
+	fmt.Printf("Issuer: %s\n\n", info.Cert.Issuer.String())
+	fmt.Printf("Link: %s\n\n", info.Link)
+	fmt.Printf("View: [Click to view](https://understandingwebpki.com/?cert=%s)\n\n", url.QueryEscape(base64.StdEncoding.EncodeToString(info.Cert.Raw)))
+	fmt.Println("")
+	fmt.Printf("| Code | Type | Details |\n")
+	fmt.Printf("|------|------|---------|\n")
+	for code, result := range info.Result.Results {
+		if result.Status == lint.Error || result.Status == lint.Warn {
+			fmt.Printf("| %s | %s | %s |\n", code, result.Status, result.Details)
+		}
+	}
 }
