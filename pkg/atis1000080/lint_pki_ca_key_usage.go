f11e5d713a0b98508130276d04f6751d22d7d66a
microshine <microshine@mail.ru>
2022-10-13 23:04:13 +0600

    fix(lint): change status for n_shaken_pki_ca_key_usage

--- pkg/atis1000080/lint_pki_ca_key_usage.go
+++ pkg/atis1000080/lint_pki_ca_key_usage.go
@@ -10,8 +10,8 @@ type pkiCaKeyUsage struct{}
 
 func init() {
 	lint.RegisterLint(&lint.Lint{
-		Name:          "e_shaken_pki_ca_key_usage",
-		Description:   "For CA certificates, the Key Usage extension shall contain a single key usage value of keyCertSign",
+		Name:          "n_shaken_pki_ca_key_usage",
+		Description:   "For CA certificates, the Key Usage extension should contain a single key usage value of keyCertSign",
 		Citation:      PKI_Citation,
 		Source:        SHAKEN,
 		EffectiveDate: util.ZeroDate,
@@ -32,8 +32,8 @@ func (*pkiCaKeyUsage) CheckApplies(c *x509.Certificate) bool {
 func (*pkiCaKeyUsage) Execute(c *x509.Certificate) *lint.LintResult {
 	if c.KeyUsage != x509.KeyUsageCertSign {
 		return &lint.LintResult{
-			Status:  lint.Error,
-			Details: "For CA certificates, the Key Usage extension shall contain a single key usage value of keyCertSign",
+			Status:  lint.Notice,
+			Details: "For CA certificates, the Key Usage extension should contain a single key usage value of keyCertSign",
 		}
 	}
 
